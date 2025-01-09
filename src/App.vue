ab0a034fdea1ce7a4d7531de4fa262544d0a2683
chinhnguyen <chinhnguyenduc2000@gmail.com>
2025-01-09 11:24:15 +0700

    code

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full flex justify-center">
+    <img src="./assets/nyd.png" alt="" class=" h-[800px] mt-[50px]" >
+  </div>
+</template>
+
+<style scoped>
+
+</style>
